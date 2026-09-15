1d91bae3b321f1d7a6109d1c4d2a82f2c755709f
yanggoat <yanggoat@outlook.com>
2026-09-15 09:19:08 +0800

    add init project

---
+++ dsp-catalog-transform-2025/build.gradle.kts
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2025 Metaform Systems, Inc.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Metaform Systems, Inc. - initial API and implementation
+ *       Cofinity-X - make DSP versions pluggable
+ *
+ */
+
+
+plugins {
+    `java-library`
+}
+
+dependencies {
+    api(project(":spi:common:core-spi"))
+    api(project(":data-protocols:dsp:dsp-2025:dsp-spi-2025"))
+
+    implementation(project(":core:common:lib:transform-lib"))
+    implementation(project(":data-protocols:dsp:dsp-lib:dsp-catalog-lib:dsp-catalog-transform-lib"))
+
+    testImplementation(testFixtures(project(":core:common:lib:json-ld-lib")))
+}
