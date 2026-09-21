75c6faaff1bd40b7f767902f09ddfc9e491a6ba3
yanggoat <yanggoat@outlook.com>
2026-09-21 15:05:02 +0800

    add pom for project youdatax-dsp-v2025-catalog-api-http

--- youdatax-dsp-v2025-catalog-api-http/build.gradle.kts
+++ youdatax-dsp-v2025-catalog-api-http/build.gradle.kts
@@ -10,7 +10,7 @@
  *  Contributors:
  *       Metaform Systems, Inc. - initial API and implementation
  *       Cofinity-X - make DSP versions pluggable
- *
+ * \data-protocols\dsp\dsp-2025\dsp-catalog-2025\dsp-catalog-http-api-2025
  */
 
 plugins {
